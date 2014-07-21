72916e2c114e4d975cfb87ce4b1e01923c69c6c4
Ilia <ilia@ilia-Z68P-DS3.(none)>
2014-07-21 15:23:21 -0400

    secondary commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#index'
+  get 'pages/index'
+
+  get 'pages/about'
+
+  get 'pages/services'
+
+  get 'pages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
